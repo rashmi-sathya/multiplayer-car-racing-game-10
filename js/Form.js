a5be063e481b25b019f2e445fa8f59495e9cb935
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-09-08 17:33:19 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,53 @@
+class Form {
+  constructor() {
+    this.input = createInput("").attribute("placeholder", "Enter your name");
+    this.playButton = createButton("Play");
+    this.titleImg = createImg("./assets/title.png", "game title");
+    this.greeting = createElement("h2");
+  }
+
+  setElementsPosition() {
+    this.titleImg.position(120, 160);
+    this.input.position(width / 2 - 110, height / 2 - 80);
+    this.playButton.position(width / 2 - 90, height / 2 - 20);
+    this.greeting.position(width / 2 - 300, height / 2 - 100);
+  }
+
+  setElementsStyle() {
+    this.titleImg.class("gameTitle");
+    this.input.class("customInput");
+    this.playButton.class("customButton");
+    this.greeting.class("greeting");
+  }
+
+  //BP
+  hide() {
+    this.greeting.hide();
+    this.playButton.hide();
+    this.input.hide();
+  }
+
+  //BP
+  handleMousePressed() {
+    this.playButton.mousePressed(() => {
+      this.input.hide();
+      this.playButton.hide();
+      var message = `
+      Hello ${this.input.value()}
+      </br>wait for another player to join...`;
+      this.greeting.html(message);
+      playerCount += 1;
+      player.name = this.input.value();
+      player.index = playerCount;
+    player.addPlayer();//aa
+      player.updateCount(playerCount); // BP
+     player.getDistance(); //aa
+    });
+  }
+
+  display() {
+    this.setElementsPosition();
+    this.setElementsStyle();
+    this.handleMousePressed();
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,50 @@
+var canvas;
+var backgroundImage, bgImg, car1_img, car2_img, track;
+var database, gameState;
+var form, player, playerCount;
+var allPlayers, car1, car2,fuels, powerCoins;
+var cars = [];
+var fuelImage, powerCoinImage,obstacle1,obstacle1Image,obstacle2Img,obstacle2;
+//BP
+function preload() {
+  backgroundImage = loadImage("./assets/background.png");
+  car1_img = loadImage("../assets/car1.png");
+  car2_img = loadImage("../assets/car2.png");
+  track = loadImage("../assets/track.jpg");
+  fuelImage = loadImage("./assets/fuel.png");
+  powerCoinImage = loadImage("./assets/goldCoin.png");
+  lifeImage = loadImage("./assets/life.png");
+  obstacle1Image = loadImage("./assets/obstacle1.png"); 
+  obstacle2Image = loadImage("./assets/obstacle2.png");
+
+}
+
+//BP
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+ 
+}
+
+//BP
+function draw() {
+  background(backgroundImage);
+  if (playerCount === 2) {
+    game.update(1);
+  }
+
+  if (gameState === 1) {
+    game.play();
+  }
+  if (gameState === 2) {
+    game.showLeaderboard();
+    //game.end();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
